1292841afde2506d42011a48bb14191ae45477f8
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2019-11-21 15:06:44 +0000

    homepage into smaller components, finished styling menu-item

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Components Imports
+import Directory from "../../components/Directory/Directory";
+
+//Style Imports
+import "./HomePage.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  )
+}
+
+export default HomePage;
